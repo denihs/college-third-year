f1eff982e25328eb0aba0da70931ae66b8f55298
Denilson <denilsonh2014@gmail.com>
2019-04-22 15:46:52 -0400

    Trab CG

--- CG/trab/geometricTransformation.cpp
+++ CG/trab/geometricTransformation.cpp
@@ -1,19 +1,7 @@
-////////////////////////////////////////////////////////////////////////////////////
-// canvas.cpp
-//
-// This program allows the user to draw simple shapes on a canvas.
-//
-// Interaction:
-// Left click on a box on the left to select a primitive.
-// Then left click on the drawing area: once for point, twice for line or rectangle.
-// Right click for menu options.
-//
-//  Sumanta Guha.
-////////////////////////////////////////////////////////////////////////////////////
-
 #include <cstdlib>
 #include <vector>
 #include <iostream>
+#include <cmath>
 
 #ifdef __APPLE__
 #  include <GLUT/glut.h>
@@ -25,11 +13,10 @@ using namespace std;
 
 #define INACTIVE 0
 #define TRANSLATION 1
-#define POINT 1
 #define SCALE 2
-#define LINE 2
-#define RECTANGLE 3
+#define ROTATE 3
 #define NUMBERPRIMITIVES 3
+#define PI 3.14159265358979324
 
 #define SIDE_MENU_WIDTH 0.4285
 
@@ -40,47 +27,48 @@ using namespace std;
 static GLsizei width, height; // OpenGL window size.
 static float pointSize = 6.0; // Size of point
 static int primitive = INACTIVE; // Current drawing primitive.
-static int pointCount = 0; // Number of  specified points.
-static int tempX, tempY; // Co-ordinates of clicked point.
-static int isGrid = 1; // Is there grid?
 static int translationX = 0, translationY = 0;
-static int scaleX = 10, scaleY = 10;
+static int scaleX = 0, scaleY = 0;
+static int degrees = 0;
 static long font = (long)GLUT_BITMAP_9_BY_15; // Font selection.
 
 // Routine to draw a bitmap character string.
-void writeBitmapString(void *font, char *string, int x = 100)
-{
+void writeBitmapString(void *font, char *string, int x = 1000) {
     char *c;
     int i[10] = {100, 100, 100, 100, 100, 100, 100, 100, 100, 100};
     int aux = x;
-
+    
     for (c = string; *c != '\0'; c++) glutBitmapCharacter(font, *c);
-
-    if (x < 100) {
-
+    
+    if (x < 1000) {
         if (x < 0) {
             glutBitmapCharacter(font, 45);
             aux *= -1;
         }
-
+        
         if (aux >= 0 && aux < 10) {
             glutBitmapCharacter(font, aux + 48);
             return;
         }
 
-        i[0] = 1;
-        i[1] = 0;
+        int d = aux < 100 ? 10 : 100;
+        
+        for (int j = 0; j < 10, d >= 1; j++) {
+            i[j] = aux / d;
+            int rest = aux % d;
+            aux = rest;
+            d /= 10;
+        }
+
         for (int h = 0; i[h] != 100; h++) glutBitmapCharacter(font, i[h] + 48);
 
     }
 }
 
 // Point class.
-class Point
-{
+class Point {
 public:
-    Point(double xVal, double yVal)
-    {
+    Point(double xVal, double yVal) {
         x = xVal; y = yVal;
     }
     void drawPoint(void); // Function to draw a point.
@@ -100,23 +88,6 @@ void Point::drawPoint()
     glEnd();
 }
 
-// Vector of points.
-vector<Point> points;
-
-// Iterator to traverse a Point array.
-vector<Point>::iterator pointsIterator;
-
-// Function to draw all points in the points array.
-void drawPoints(void)
-{
-    // Loop through the points array drawing each point.
-    pointsIterator = points.begin();
-    while(pointsIterator != points.end() )
-    {
-        pointsIterator->drawPoint();
-        pointsIterator++;
-    }
-}
 
 // Line class.
 class Line
@@ -141,65 +112,37 @@ void Line::drawLine()
     glEnd();
 }
 
-// Vector of lines.
-vector<Line> lines;
-
-// Iterator to traverse a Line array.
-vector<Line>::iterator linesIterator;
-
-// Function to draw all lines in the lines array.
-void drawLines(void)
-{
-    // Loop through the lines array drawing each line.
-    linesIterator = lines.begin();
-    while(linesIterator != lines.end() )
-    {
-        linesIterator->drawLine();
-        linesIterator++;
-    }
-}
-
-// Rectangle class.
-class Rectangle
-{
-public:
-    Rectangle(int x1Val, int y1Val, int x2Val, int y2Val)
-    {
-        x1 = x1Val; y1 = y1Val; x2 = x2Val; y2 = y2Val;
-    }
-    void drawRectangle();
-private:
-    int x1, y1, x2, y2; // x and y co-ordinates of diagonally opposite vertices.
-};
-
-// Function to draw a rectangle.
-void Rectangle::drawRectangle()
-{
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glRectf(x1, y1, x2, y2);
-}
-
-// Vector of rectangles.
-vector<Rectangle> rectangles;
-
-// Iterator to traverse a Rectangle array.
-vector<Rectangle>::iterator rectanglesIterator;
-
-// Function to draw all rectangles in the rectangles array.
-void drawRectangles(void)
-{
-    // Loop through the rectangles array drawing each rectangle.
-    rectanglesIterator = rectangles.begin();
-    while(rectanglesIterator != rectangles.end() )
-    {
-        rectanglesIterator->drawRectangle();
-        rectanglesIterator++;
-    }
-}
-
 float getVariant(float V, int U) {
     return (1 + ((V * U) / 10));
 }
+
+// Circle class.
+class Circle {
+public:
+    Circle(double x1Val, double y1Val, double r, int nv)
+   {
+	  x1 = x1Val; y1 = y1Val; R = r; numVertices = nv;
+   }
+   void drawCircle();
+private:
+   double x1, y1, R; 
+   int numVertices;
+};
+
+// Function to draw a circle.
+void Circle::drawCircle() {
+
+    float t = 0;
+
+    glBegin(GL_LINE_LOOP);
+        for(int i = 0; i < numVertices; ++i)  {
+            glColor3f(0.0, 0.0, 0.0);
+            glVertex3f(x1 + R * cos(t), y1 + R * sin(t), 0.0);
+            t += 2 * PI / numVertices;
+        }
+    glEnd();
+}
+
 
 // Function to draw translation selection box in left selection area.
 void drawTranslationBox(void) {
@@ -306,6 +249,54 @@ void drawScaleBox(void) {
 
 }
 
+// Function to draw rotate selection box in left selection area.
+void drawRotateBox(void) {
+    float w1 = 0.0,
+            w2 = SIDE_MENU_WIDTH*width,
+            h1 = 0.1*height,
+            h2 = 0.4 * height;
+
+
+    if (primitive == ROTATE) glColor3f(1.0, 1.0, 1.0); // Highlight.
+    else glColor3f(0.8, 0.8, 0.8); // No highlight.
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glRectf(w1, h1, w2, h2);
+
+    // Draw black boundary.
+    glColor3f(0.0, 0.0, 0.0);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glRectf(w1, h1, w2, h2);
+
+    glColor3f(0.0, 0.0, 0.0);
+    glRasterPos3f(width * 0.01, height * 0.35, 0.0);
+    writeBitmapString((void*)font, "Rotate");
+
+    glRasterPos3f(width * 0.01, height * 0.25, 0.0);
+    writeBitmapString((void*)font, "G = ", degrees);
+
+    double centerX = SIDE_MENU_WIDTH * 0.7 * width;
+    double centerY = 0.25*height;
+
+    double R = 60.0;
+    double nVert = 360;
+
+    Circle c = Circle(centerX, centerY, R, nVert);
+    c.drawCircle();
+
+    Line l = Line(centerX, centerY, centerX + R, centerY);
+    l.drawLine();
+
+    double t = (2 * PI / nVert) * degrees;
+    double xPoint = centerX + R * cos(t), yPoint = centerY + R * sin(t);
+
+    glColor3f(1.0, 0.0, 0.0);
+    Point p = Point(xPoint, yPoint);
+    p.drawPoint();
+
+    Line lr = Line(centerX, centerY, xPoint, yPoint);
+    lr.drawLine();
+}
+
 // Function to draw Cartesian plane selection
 void drawCartesianPlace(void) {
     float w1 = 0.0,
@@ -336,8 +327,6 @@ void drawCartesianPlace(void) {
     double coordY = centerY * getVariant(variantY, translationY);
 
     int cubeScale = 5;
-
-    double  dist =
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     glBegin(GL_POLYGON);
@@ -374,16 +363,6 @@ void drawInactiveArea(void)
     glRectf(0.0, 0.0, SIDE_MENU_WIDTH*width, (1 - NUMBERPRIMITIVES*0.3)*height);
 }
 
-// Function to draw temporary point.
-void drawTempPoint(void)
-{
-    glColor3f(1.0, 0.0, 0.0);
-    glPointSize(pointSize);
-    glBegin(GL_POINTS);
-    glVertex3f(tempX, tempY, 0.0);
-    glEnd();
-}
-
 // Drawing routine.
 void drawScene(void)
 {
@@ -393,13 +372,8 @@ void drawScene(void)
     drawCartesianPlace();
     drawTranslationBox();
     drawScaleBox();
+    drawRotateBox();
     drawInactiveArea();
-
-    drawPoints();
-    drawLines();
-    drawRectangles();
-    if ( ((primitive == LINE) || (primitive == RECTANGLE)) &&
-         (pointCount == 1) ) drawTempPoint();
 
     glutSwapBuffers();
 }
@@ -408,7 +382,7 @@ void drawScene(void)
 void pickPrimitive(int y)
 {
     if ( y < (1- NUMBERPRIMITIVES*0.3)*height ) primitive = INACTIVE;
-    else if ( y < (1 - 2*0.3)*height ) primitive = RECTANGLE;
+    else if ( y < (1 - 2*0.3)*height ) primitive = ROTATE;
     else if ( y < (1 - 1*0.3)*height ) primitive = SCALE;
     else primitive = TRANSLATION;
 }
@@ -416,50 +390,15 @@ void pickPrimitive(int y)
 // The mouse callback routine.
 void mouseControl(int button, int state, int x, int y)
 {
-    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-    {
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
         y = height - y; // Correct from mouse to OpenGL co-ordinates.
 
         // Click outside canvas - do nothing.
         if ( x < 0 || x > width || y < 0 || y > height ) ;
 
             // Click in left selection area.
-        else if ( x < SIDE_MENU_WIDTH*width )
-        {
+        else if ( x < SIDE_MENU_WIDTH*width ) {
             pickPrimitive(y);
-            pointCount = 0;
-        }
-
-            // Click in canvas.
-        else
-        {
-            if (primitive == POINT) points.push_back( Point(x,y) );
-            else if (primitive == LINE)
-            {
-                if (pointCount == 0)
-                {
-                    tempX = x; tempY = y;
-                    pointCount++;
-                }
-                else
-                {
-                    lines.push_back( Line(tempX, tempY, x, y) );
-                    pointCount = 0;
-                }
-            }
-            else if (primitive == RECTANGLE)
-            {
-                if (pointCount == 0)
-                {
-                    tempX = x; tempY = y;
-                    pointCount++;
-                }
-                else
-                {
-                    rectangles.push_back( Rectangle(tempX, tempY, x, y) );
-                    pointCount = 0;
-                }
-            }
         }
     }
     glutPostRedisplay();
@@ -507,10 +446,14 @@ void  keyControl(int key, int x, int y) {
         case GLUT_KEY_UP:
             if (translationY < 10 && primitive == TRANSLATION) translationY++;
             if (scaleY < 10 && primitive == SCALE) scaleY++;
+            if (degrees == 360 && primitive == ROTATE) degrees = 0;
+            if (degrees < 360 && primitive == ROTATE) degrees += 15;
             break;
         case GLUT_KEY_DOWN:
             if (translationY > -10 && primitive == TRANSLATION) translationY--;
             if (scaleY > 0 && primitive == SCALE) scaleY--;
+            if (degrees == 0 && primitive == ROTATE) degrees = 360;
+            if (degrees > 0 && primitive == ROTATE) degrees = degrees -= 15;
             break;
         case GLUT_KEY_LEFT:
             if (translationX > -10 && primitive == TRANSLATION) translationX--;
@@ -526,57 +469,11 @@ void  keyControl(int key, int x, int y) {
     glutPostRedisplay();
 }
 
-// Clear the canvas and reset for fresh drawing.
-void clearAll(void)
-{
-    points.clear();
-    lines.clear();
-    rectangles.clear();
-    primitive = INACTIVE;
-    pointCount = 0;
-}
-
-// The right button menu callback function.
-void rightMenu(int id)
-{
-    if (id==1)
-    {
-        clearAll();
-        glutPostRedisplay();
-    }
-    if (id==2) exit(0);
-}
-
-// The sub-menu callback function.
-void grid_menu(int id)
-{
-    if (id==3) isGrid = 1;
-    if (id==4) isGrid = 0;
-    glutPostRedisplay();
-}
-
-// Function to create menu.
-void makeMenu(void)
-{
-    int sub_menu;
-    sub_menu = glutCreateMenu(grid_menu);
-    glutAddMenuEntry("On", 3);
-    glutAddMenuEntry("Off",4);
-
-    glutCreateMenu(rightMenu);
-    glutAddSubMenu("Grid", sub_menu);
-    glutAddMenuEntry("Clear",1);
-    glutAddMenuEntry("Quit",2);
-    glutAttachMenu(GLUT_RIGHT_BUTTON);
-}
-
 // Routine to output interaction instructions to the C++ window.
 void printInteraction(void)
 {
     cout << "Interaction:" << endl;
-    cout << "Left click on a box on the left to select a primitive." << endl
-         << "Then left click on the drawing area: once for point, twice for line or rectangle." << endl
-         << "Right click for menu options." <<  endl;
+    cout << "Use the side menu to iteract with the square" << endl;
 }
 
 // Main routine.
@@ -594,8 +491,6 @@ int main(int argc, char **argv)
     glutKeyboardFunc(keyInput);
     glutMouseFunc(mouseControl);
     glutSpecialFunc(keyControl);
-    makeMenu(); // Create menu.
-
     glutMainLoop();
 
     return 0;
