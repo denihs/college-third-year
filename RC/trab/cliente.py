45d29ba1015db62efc1ca5e34057a0509ac78cfc
denyhs <denilsonh2014@gmail.com>
2019-06-23 17:11:10 -0400

    Permitindo o cliente entrar com dados

--- RC/trab/cliente.py
+++ RC/trab/cliente.py
@@ -8,45 +8,65 @@ if len(sys.argv) != 3:
     print("Numero de parâmetros inválidos!")
     exit()
 
-dadosGravar = {
-    "acao": "D",
-    "valor": 4999,
-    "tipoCombustivel": 1,
-    "coordenadas": "(-22.21532688695198;-54.75933148596293)"
-}
+def tratarEntrada(d):
+    if d.lower() == 'sair':
+        return d
 
-dadosPesquisa = {
-    "acao": "P",
-    "raio": 1,
-    "tipoCombustivel": 2,
-    "coordenadas": "(-22.21532688695198;-54.75933148596293)"
-}
+    dados = d.replace(' ', '').split(',')
+    if len(dados) != 4:
+        print("Numero inválido de parâmetros\n")
+        return None
+
+    coordenadas = dados[3].split(';')
+    if len(coordenadas) != 2 and ('(' not in coordenadas or ')' not in coordenadas):
+        print("Coordenadas inválidas\n")
+        return None
+
+    dadosEnvio = {
+        "acao": dados[0],
+        "tipoCombustivel": dados[2],
+        "coordenadas": dados[3]
+    }
+
+    if dadosEnvio["acao"].lower() == "d":
+        dadosEnvio["valor"] = dados[1]
+    else:
+        dadosEnvio["raio"] = dados[1]
+
+    return dadosEnvio
+
 
 def enviarDados(sock, IP, PORTA):
-    resp = input("Enviar dados? s / n: ")
+    entrada = True
 
     auxPacote = novoPacote()
 
-    while resp == 's':
-        seq = auxPacote["seq"] + 1
-        pacote = novoPacote(dados=dadosPesquisa, seq=seq, proxSeq = seq + 1)
+    while entrada != 'sair':
+        entrada = input("Entrada: ")
 
-        resp = transmitir(sock, pacote, IP, PORTA)
+        dados = tratarEntrada(entrada)
 
-        if resp:
-            dados = resp["dados"]
-            del resp["dados"]
-            print("\nPacote recebido: {}".format(json.dumps(resp, indent=2)))
-            print("\nDados do posto encontrado")
-            print("------------------------------")
-            print(dados)
-            print("\n")
+        if dados and dados != 'sair':
+            seq = auxPacote["seq"] + 1
+            pacote = novoPacote(dados=dados, seq=seq, proxSeq = seq + 1)
 
-            auxPacote = pacote
-        else:
-            print("Falha ao enviar os dados!")
-            print("\n")
-        resp = input("Enviar dados? s / n: ")
+            resp = transmitir(sock, pacote, IP, PORTA)
+
+            if resp:
+                respDados = resp["dados"]
+                del resp["dados"]
+                print("\nPacote recebido: {}".format(json.dumps(resp, indent=2)))
+                if dados["acao"].lower() == 'p':
+                    print("\nDados do posto encontrado")
+                    print("------------------------------")
+                print(respDados)
+                print("\n")
+
+                auxPacote = pacote
+            else:
+                print("Falha ao enviar os dados!")
+                print("\n")
+
 
 if __name__ == '__main__':
     UDP_IP = sys.argv[1]
