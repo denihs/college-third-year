02fe659caf4e44f7ae2fbe85f9191bd5c3a3c453
denyhs <denilsonh2014@gmail.com>
2019-09-23 18:58:15 -0400

    tc trabalho

--- TC/automato.c
+++ TC/automato.c
@@ -1,44 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct Node{
- int num;
+ char elem;
  struct Node *prox;
 };
 typedef struct Node node;
 
-int tam;
-
-int menu(void);
-void inicia(node *PILHA);
-void opcao(node *PILHA, int op);
-void exibe(node *PILHA);
-void libera(node *PILHA);
-void push(node *PILHA);
-node *pop(node *PILHA);
-
-
-int main(int argc, char *argv[])
-{
- node *PILHA = (node *) malloc(sizeof(node));
- if(!PILHA){
-  printf("Sem memoria disponivel!\n");
-  exit(1);
- }else{
- inicia(PILHA);
- int opt;
-
-    printf("%s\n", argv[1]);
-
- free(PILHA);
- return 0;
- }
-}
 
 void inicia(node *PILHA)
 {
  PILHA->prox = NULL;
- tam=0;
 }
 
 int vazia(node *PILHA)
@@ -49,50 +22,23 @@ int vazia(node *PILHA)
   return 0;
 }
 
-node *aloca()
+node *aloca(char elem)
 {
  node *novo=(node *) malloc(sizeof(node));
  if(!novo){
   printf("Sem memoria disponivel!\n");
   exit(1);
  }else{
-  printf("Novo elemento: "); scanf("%d", &novo->num);
+  novo->elem = elem;
   return novo;
  }
-}
-
-
-void exibe(node *PILHA)
-{
- if(vazia(PILHA)){
-  printf("PILHA vazia!\n\n");
-  return ;
- }
-
- node *tmp;
- tmp = PILHA->prox;
- printf("PILHA:");
- while( tmp != NULL){
-  printf("%5d", tmp->num);
-  tmp = tmp->prox;
- }
- printf("\n        ");
- int count;
- for(count=0 ; count < tam ; count++)
-  printf("  ^  ");
- printf("\nOrdem:");
- for(count=0 ; count < tam ; count++)
-  printf("%5d", count+1);
-
-
- printf("\n\n");
 }
 
 void libera(node *PILHA)
 {
  if(!vazia(PILHA)){
   node *proxNode,
-     *atual;
+          *atual;
 
   atual = PILHA->prox;
   while(atual != NULL){
@@ -103,9 +49,21 @@ void libera(node *PILHA)
  }
 }
 
-void push(node *PILHA)
+void showStack(node * p) {
+ if(vazia(p)) {
+  printf("Pilha esvaziada! \n");
+ }
+ node * temp = p;
+ while (temp) {
+  printf("%c ", temp->elem);
+  temp = temp->prox;
+ }
+ printf("\n");
+}
+
+void push(node *PILHA, char elem)
 {
- node *novo=aloca();
+ node *novo=aloca(elem);
  novo->prox = NULL;
 
  if(vazia(PILHA))
@@ -118,8 +76,8 @@ void push(node *PILHA)
 
   tmp->prox = novo;
  }
- tam++;
-}   
+ showStack(PILHA);
+}
 
 
 node *pop(node *PILHA)
@@ -129,7 +87,7 @@ node *pop(node *PILHA)
   return NULL;
  }else{
   node *ultimo = PILHA->prox,
-              *penultimo = PILHA;
+          *penultimo = PILHA;
 
   while(ultimo->prox != NULL){
    penultimo = ultimo;
@@ -137,7 +95,72 @@ node *pop(node *PILHA)
   }
 
   penultimo->prox = NULL;
-  tam--;
+  showStack(PILHA);
   return ultimo;
  }
+}
+
+node *verifyWord(char * word, int size) {
+ char a = 'a';
+ char b = 'b';
+ char letter;
+ int isValid;
+ int isBTime = 0;
+
+ node *PILHA = aloca(' ');
+
+ inicia(PILHA);
+
+ if (word[0] != a) {
+  printf("Palavra não aceita! A palavra deve começar com o caracter 'a'.\n");
+  return PILHA;
+ }
+
+
+ for(int i = 0; i < size; i++) {
+   letter = word[i];
+   isValid = vazia(PILHA);
+
+   if (letter == a && !isBTime) {
+    push(PILHA, a);
+   } else if (letter == b && !isValid) {
+    isBTime = 1;
+    pop(PILHA);
+   } else {
+    if (isValid) printf("Palavra não aceita! A quantidade de B's é maior que a de A's,\n");
+    else printf("Palavra não aceita! O caracter  '%c' não faz parte do alfabeto.\n", letter);
+
+    return PILHA;
+   }
+ }
+
+ if (vazia(PILHA)) {
+  printf("Palavra aceita\n");
+  return PILHA;
+ }
+ printf("Palavra não aceita! A quantidade de A's é maior que a de B's.\n");
+ return PILHA;
+}
+
+int main(int argc, char *argv[])
+{
+
+ char * word = argv[1];
+ if (!word) {
+  printf("Passe a palavra como argumento\n");
+  return 0;
+ }
+
+ int wordSize = strlen(word);
+
+if (wordSize < 2) {
+ printf("A palavra deve ter um tamanho maior ou igual a dois\n");
+ return 0;
+}
+
+
+ int opt;
+
+ libera(verifyWord(word, wordSize));
+ return 0;
 }
